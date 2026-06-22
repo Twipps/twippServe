985779876dc5a54b6dbdc4a7f902d403b8166ba8
Twipps <jsuchovic@gmail.com>
2026-06-22 15:59:20 -0700

    started css and html routing

--- src/server/twippServe.pas
+++ src/server/twippServe.pas
@@ -1,11 +1,80 @@
 program twippServe;
 {$mode objfpc}{$H+}
 
-uses SysUtils, fphttpapp, httpdefs, httproute;
+uses Classes, SysUtils, fphttpapp, httpdefs, httproute;
+
+function fetchStyle(styleSheetID: int8): string;
+var
+    rCss: string;
+    stringList: TStringList; {things in the std lib seem to start with T}
+
+begin
+    {create new obj}
+    stringList := TStringList.Create;
+
+    try
+    case styleSheetID of
+      0:
+      begin
+            stringList.LoadFromFile('../../web/css/backgroundStyle.css');
+            rCss := stringList.Text;
+        end;
+    else
+        writeLn('Fetch Error: Requested styleSheetID not found.');
+    end;
+
+    finally
+    {free mem}
+    stringList.Free;
+    end;
+    result := rCss;
+end;
+
+function fetchPage(pageID: int8): string;
+var
+    rHtml: string;
+    stringList: TStringList; {things in the std lib seem to start with T}
+
+begin
+    {create new obj}
+    stringList := TStringList.Create;
+
+    try
+    case pageID of
+      0:
+      begin
+            stringList.LoadFromFile('../../web/html/twipp.html');
+            rHtml := stringList.Text;
+        end;
+    else
+        writeLn('Fetch Error: Requested pageID not found.');
+    end;
+
+    finally
+    {free mem}
+    stringList.Free;
+    end;
+    result := rHtml;
+end;
+
+procedure routeBackgroundStyle(request: TRequest; response: TResponse);
+const
+    styleSheetID: int8 = 0;
+
+begin
+    response.Code := 200;
+    writeLn(request.Method + ': Serving styleSheetID ' + IntToStr(styleSheetID) + ' to ' + request.RemoteAddr);
+    response.Content := fetchStyle(styleSheetID);
+end;
 
 procedure routeMain(request: TRequest; response: TResponse);
+const 
+    routeID: int8 = 0;
+
 begin
-    response.Content := '<!DOCTYPE html><html><head><title>TwippsMain</title></head><body>twipps.com</body></html>';
+    response.Code := 200;
+    writeLn(request.Method + ': Serving pageID ' + IntToStr(routeID) + ' to ' + request.RemoteAddr);
+    response.Content := fetchPage(routeID);
 end;
 
 var 
@@ -15,6 +84,9 @@ begin
     {Registers routes for the handler to manage}
     HTTPRouter.RegisterRoute('/', @routeMain);
     writeln('----- Starting twippServe -----');
+
+    {serves the background css}
+    HTTPRouter.RegisterRoute('/css/backSgroundStyle.css', @routeBackgroundStyle);
 
     {Sets up the port}
     Application.Port := 700;
